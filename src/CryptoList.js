c00f093fc95614e0e4505f973e25088e65871451
Alen Medakovic <alenmedakovic@Alens-MacBook-Pro.local>
2022-12-30 19:41:34 +0100

    Add Image, Borders and other styling.

--- src/CryptoList.js
+++ src/CryptoList.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import { useState } from "react";
 import "@fortawesome/fontawesome-free/css/all.css";
+import candlePhoto from "./photos/candles.png"
+
 
 
 
@@ -23,12 +25,16 @@ function CryptoList(cryptoResponseObject) {
   }
 
   return (
-    <div className="container relative shadow-md sm:rounded-lg ">
-      <h1 className="mb-28 ">Top 100 Cryptocurrencies by rank</h1>
+    <div style={{backgroundImage: `url(${candlePhoto})`,
+    backgroundPosition: "100px 0px",
+  }}
+   className="container mx-auto w-full h-full
+     relative sm:rounded-lg">
+      <h1 className="mb-52 mt-16 font-extrabold text-center text-2xl">Top 100 cryptocurrencies by rank</h1>
       <table className="text-center w-full text-sm text-gray-600">
         <thead className="bg-white flex text-black w-full border-black border-2
         bg-gradient-to-r
-        from-white to-pink-100">
+        from-white">
           <tr className="flex w-full mb-4">
           <th className="p-4 w-1/4 py-3 px-6">Rank</th>
             <th className="p-4 w-1/4 py-3 px-6">Symbol</th>
@@ -38,7 +44,7 @@ function CryptoList(cryptoResponseObject) {
             <th className="p-4 w-1/4 py-3 px-6">Market Cap (USD)</th>
           </tr>
         </thead>
-        <tbody className="bg-grey-light flex flex-col items-center justify-between overflow-y-scroll w-full" style={{height: '50vh;'}}>
+        <tbody className="border-2 border-black flex flex-col items-center justify-between w-full">
           {Object.keys(rates).map(key => {
             const percentChange = rates[key].percent_change_24h;
             let percentColor = "p-4 w-1/4";
@@ -62,19 +68,19 @@ function CryptoList(cryptoResponseObject) {
 
             return (
               <tr key={key} className="flex w-full  bg-white border-b bg-gradient-to-r
-               from-white to-pink-100 dark:border-gray-700">
+               from-white  dark:border-gray-700">
                 <td className="p-4 w-1/4 py-4 px-6 font-semibold">{rates[key].rank}<span
                 className="mx-2">&nbsp;</span>
                 <a href="#" onClick={() => handleClickHeart(key)}>
                   <i className={`fas fa-heart ${activeRows[key] ? "text-indigo-700":
-                  "text-gray-500"}`}></i>
+                  "text-gray-300"}`}></i>
                 </a>
                 </td>
                 <td className="p-4 w-1/4 py-4 px-6 font-bold text-gray-900">{rates[key].symbol}</td>
-                <td className="p-4 w-1/4 py-4 px-6 font-semibold">{rates[key].name}</td>
-                <td className="p-4 w-1/4 py-4 px-6">${shortenedPrice}</td>
+                <td className="p-4 w-1/4 py-4 px-6 ">{rates[key].name}</td>
+                <td className="p-4 w-1/4 py-4 px-6 font-semibold text-gray-900">${shortenedPrice}</td>
                 <td className={percentColor}>{rates[key].percent_change_24h + "%"}</td>
-                <td className="p-4 w-1/4 py-4 px-6">{marketCapInBillions} B</td>
+                <td className="p-4 w-1/4 py-4 px-6 font-semibold text-gray-900">{marketCapInBillions} B</td>
               </tr>
             );
           })}
