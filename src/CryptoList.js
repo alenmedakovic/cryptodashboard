2b0c4f644945063d7cd9d88e9a0cde7ab6c23086
Alen Medakovic <alenmedakovic@Alens-MacBook-Pro.local>
2022-12-30 18:09:13 +0100

    Add more styling,functional favorite button

--- src/CryptoList.js
+++ src/CryptoList.js
@@ -1,27 +1,41 @@
 
 import React from 'react';
+import { useState } from "react";
+import "@fortawesome/fontawesome-free/css/all.css";
 
 
 
 function CryptoList(cryptoResponseObject) {
   const { rates } = cryptoResponseObject;
 
+  const [activeRows, setActiveRows] = useState({});
+
+  const handleClickHeart = (key) => {
+    setActiveRows((prevActiveRows) => ({
+      ...prevActiveRows,
+      [key]: !prevActiveRows[key],
+    }));
+  };
+
+
   if (Object.keys(rates).length === 0) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div className="container">
-      <h1 className="mb-8">My Table</h1>
-      <table className="text-left w-full">
-        <thead className="bg-black flex text-white w-full">
+    <div className="container relative shadow-md sm:rounded-lg ">
+      <h1 className="mb-28 ">Top 100 Cryptocurrencies by rank</h1>
+      <table className="text-center w-full text-sm text-gray-600">
+        <thead className="bg-white flex text-black w-full border-black border-2
+        bg-gradient-to-r
+        from-white to-pink-100">
           <tr className="flex w-full mb-4">
-          <th className="p-4 w-1/4">Rank</th>
-            <th className="p-4 w-1/4">Symbol</th>
-            <th className="p-4 w-1/4">Name</th>
-            <th className="p-4 w-1/4">Price (USD)</th>
-            <th className="p-4 w-1/4">Change (24h)</th>
-            <th className="p-4 w-1/4">Market Cap (USD)</th>
+          <th className="p-4 w-1/4 py-3 px-6">Rank</th>
+            <th className="p-4 w-1/4 py-3 px-6">Symbol</th>
+            <th className="p-4 w-1/4 py-3 px-6">Name</th>
+            <th className="p-4 w-1/4 py-3 px-6">Price (USD)</th>
+            <th className="p-4 w-1/4 py-3 px-6">Change (24h)</th>
+            <th className="p-4 w-1/4 py-3 px-6">Market Cap (USD)</th>
           </tr>
         </thead>
         <tbody className="bg-grey-light flex flex-col items-center justify-between overflow-y-scroll w-full" style={{height: '50vh;'}}>
@@ -47,13 +61,20 @@ function CryptoList(cryptoResponseObject) {
             
 
             return (
-              <tr key={key} className="flex w-full mb-4">
-                <td className="p-4 w-1/4">{rates[key].rank}</td>
-                <td className="p-4 w-1/4">{rates[key].symbol}</td>
-                <td className="p-4 w-1/4">{rates[key].name}</td>
-                <td className="p-4 w-1/4">${shortenedPrice}</td>
+              <tr key={key} className="flex w-full  bg-white border-b bg-gradient-to-r
+               from-white to-pink-100 dark:border-gray-700">
+                <td className="p-4 w-1/4 py-4 px-6 font-semibold">{rates[key].rank}<span
+                className="mx-2">&nbsp;</span>
+                <a href="#" onClick={() => handleClickHeart(key)}>
+                  <i className={`fas fa-heart ${activeRows[key] ? "text-indigo-700":
+                  "text-gray-500"}`}></i>
+                </a>
+                </td>
+                <td className="p-4 w-1/4 py-4 px-6 font-bold text-gray-900">{rates[key].symbol}</td>
+                <td className="p-4 w-1/4 py-4 px-6 font-semibold">{rates[key].name}</td>
+                <td className="p-4 w-1/4 py-4 px-6">${shortenedPrice}</td>
                 <td className={percentColor}>{rates[key].percent_change_24h + "%"}</td>
-                <td className="p-4 w-1/4">{marketCapInBillions} B</td>
+                <td className="p-4 w-1/4 py-4 px-6">{marketCapInBillions} B</td>
               </tr>
             );
           })}
